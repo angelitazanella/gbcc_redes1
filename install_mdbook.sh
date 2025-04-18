cb9ed8741d3244c960f12b74b01be39c277b13a2
Angelita Rettore de Araujo <angelita.zanella@ifc.edu.br>
2025-04-18 08:37:47 -0300

    Adicionando arquivo via LFS

--- install_mdbook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-`sudo apt install cargo`
-`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`
-`source "$HOME/.cargo/env"`
--- integracao.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf ../2022-gbcc_redes1/livro/*
-cp livro/src/* ../2022-gbcc_redes1/livro
